eb909c71d391e5dd78b4dbcfa51765a9c76630b3
MATHEUS SARTI BRUNELLI <m.brunelli@labcetec.net>
2024-11-08 17:12:57 -0300

    Criando sistema de autenticação

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GestaoImagem from './views/GestaoImagem.jsx';
+import TelaLogin from './views/TelaLogin.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<GestaoImagem />} />
+        <Route path='/login' element={<TelaLogin />} />
       </Routes>
     </Router>
 
--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import fileUpload from 'express-fileupload';
 import cors from 'cors';
 import {apagarImagem, criarImagem, downloadImagem, editarImagem, mostrarImagens, mostrarUmaImagem} from './controllers/ImagemController.js'
+import { criarUsuario, logarUsuario, mostrarUmUsuario, mostrarUsuario } from './controllers/UsuarioController.js';
+import { readUsuario } from './models/UsuarioModel.js';
 
 
 const app = express();
@@ -24,6 +26,21 @@ app.get('/imagem', mostrarImagens);
 app.put('/imagem/:id_imagem', editarImagem);
 app.delete('/imagem/:id_imagem',apagarImagem);
 app.get('/imagem/:id_imagem', mostrarUmaImagem);
+
+
+//CRUD USUÁRIO
+
+app.post('/usuario',criarUsuario);
+app.get('/usuario', mostrarUsuario);
+app.get('/usuario/:id_usuario',mostrarUmUsuario);
+
+
+
+//efetuar login
+
+app.post('/login', logarUsuario);
+
+
 
 app.listen(porta, ()=>{
     console.log(`API Rodando na porta ${porta}`)
